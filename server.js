6b29873f8b7ef3457b49825621e2f0bcacc9cf03
Hui Wen <huiwen_28@hotmail.com>
2018-04-04 12:55:40 +0800

    Changes for first version

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const router = express.Router(); // get an instance of the express Router
 
 // test route (accessed at GET http://localhost:8080/api)
 router.get("/", function(req, res) {
-  res.json({ message: "Hello! welcome to the mini Github Repositories api!" });
+  res.json({ message: "Hello! Welcome to the mini Github Repositories api!" });
 });
 
 router.route("/users").get(function(req, res) {
@@ -43,63 +43,40 @@ router.route("/users").get(function(req, res) {
   });
 });
 
-router.route("/users/:user/repos").get(function(req, res) {
-  let userq = req.params.user;
-  Repo.find({ user: userq }, { _id: 0, __v: 0 }, function(err, user) {
-    if (err) res.send(err);
-    res.json(user);
-  });
-});
-
-router.route("/users/:user/repos/:reponame").get(function(req, res) {
-  let userq = req.params.user;
-  let repoq = req.params.reponame;
-  Repo.findOne({ user: userq, name: repoq }, { _id: 0, __v: 0 }, function(
-    err,
-    repo
-  ) {
-    if (err) res.send(err);
-    res.json(repo);
-  });
-});
-
-// If there is > 1 value for a field query e.g. ?user=facebook&user=airbnb, only the first one will be taken into account
 router.route("/repos").get(function(req, res) {
   let data = [];
 
-  let q = {};
   if (req.query.sort) {
-    let value = req.query.sort;
-    if (Array.isArray(value)) {
-      for (let i in value) {
-        let field = value[i].split("-")[0];
-        let way = value[i].split("-")[1];
-        q[field] = way;
-      }
-    } else {
-      let field = value.split("-")[0];
-      let way = value.split("-")[1];
-      q[field] = way;
-    }
-  }
+    let field = req.query.sort.split("-")[0];
+    let way = req.query.sort.split("-")[1];
 
-  let fullq = {};
-  for (let key in req.query) {
-    if (key === "sort") continue;
-    let value = req.query[key];
-    if (Array.isArray(req.query[key])) {
-      value = req.query[key][0];
-    }
-    fullq[key] = value.toLowerCase();
-  }
-  console.log(fullq, q);
+    let q = {};
+    q[field] = way;
 
-  Repo.find(fullq)
-    .sort(q)
-    .exec(function(err, data) {
+    Repo.find({}, { _id: 0, __v: 0 })
+      .sort(q)
+      .exec(function(err, data) {
+        if (err) res.send(err);
+        res.json(data);
+      });
+  } else {
+    Repo.find({}, { _id: 0, __v: 0 }, function(err, data) {
       if (err) res.send(err);
+
+      let newData;
+      for (let key in req.query) {
+        console.log(key, req.query[key]);
+        let value = req.query[key].toLowerCase();
+
+        newData = data.filter(obj => {
+          let currKey = obj[key] + "" || "";
+          return currKey.toLowerCase() === value;
+        });
+        data = newData;
+      }
       res.json(data);
     });
+  }
 });
 
 // REGISTER OUR ROUTES -------------------------------
